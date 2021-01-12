ff11892b49c421d13436dc3c8b44c966b5e45478
oogioogi <detacl73@gmail.com>
2021-01-12 11:31:45 +0900

    -1

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -9,11 +9,91 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calculatorCollectionView: UICollectionView!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var calculatorHeightConstraint: NSLayoutConstraint!
+    
+    let numbers = [
+        ["C","()","%","÷"],
+        ["7","8","9","×"],
+        ["4","5","6","−"],
+        ["1","2","3","+"],
+        ["","0",".","="],
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        calculatorCollectionView.delegate = self
+        calculatorCollectionView.dataSource = self
+        calculatorCollectionView.register(CalculatorViewCell.self, forCellWithReuseIdentifier: "cellId")
+        calculatorHeightConstraint.constant = view.frame.width * 1.4
+        calculatorCollectionView.backgroundColor = .clear
+        
+        view.backgroundColor = .black
+        
     }
 
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return numbers.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numbers[section].count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = calculatorCollectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as? CalculatorViewCell else { return UICollectionViewCell() }
+        cell.numberLabel.text = self.numbers[indexPath.section][indexPath.row]
+        return cell
+    }
+    
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: collectionView.frame.width, height: 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.frame.width - 3 * 10) / 4
+        return .init(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+}
+
+class CalculatorViewCell: UICollectionViewCell {
+    
+    let numberLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .boldSystemFont(ofSize: 32)
+        label.clipsToBounds = true
+        label.backgroundColor = .orange
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(numberLabel)
+        
+        numberLabel.frame.size = self.frame.size
+        numberLabel.layer.cornerRadius = self.frame.height / 2
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
